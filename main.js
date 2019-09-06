32fd1f5f7b2e1222c01dc38c06c7a730bf5710de
Machin3ry <samsonnnamdi88@gmail.com>
2019-09-06 01:52:26 +0100

    Latest Commit

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function Redirect() {
-    window.location.assign("./menu/menu.html");
-}
+//Validating empty fields
+function check_empty(){
+	if (document.getElementById('name').value ==""||document.getElementById('email').value ==""||document.getElementById('msg').value == ""||document.getElementById('title').value == ""){
+		alert("Fill all fields !");
+	}else{
+		document.getElementById('fform').submit();
+		alert("Thanks for dropping by, I'll get back to you within the next 24hours");
+	}
+	}
+	//fuction to display popup
+	function div_show(){
+		document.getElementById('mform').style.display = "block";
+	}
+	//function to hide popup
+	function div_hide(){
+		document.getElementById('mform').style.display = "none";
+	};
